f3e0f1b550cf574970ac36b32c21b2c98f5aa6bb
Preesha Agrawal <cs5240888@cse.iitd.ac.in>
2025-05-17 21:59:54 +0530

    Create neuron.py
    single neuron along with a usecase

---
+++ neuron.py
@@ -0,0 +1,17 @@
+import numpy as np
+def sigmoid(x):
+  return 1/(1+np.exp(-x))
+
+class Neuron():
+  def __init__(self,weights,bias):
+    self.weights=weights
+    self.bias=bias
+
+  def feedforward(self, inputs):
+    return sigmoid(np.dot(self.weights,inputs)+self.bias)
+
+weights=np.array([0,1])
+bias=4
+n=Neuron(weights,bias)
+x=np.array([2,3])
+print(n.feedforward(x))
